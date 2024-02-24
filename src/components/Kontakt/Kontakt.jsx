faed693871d9dca5d7e60994f021fbbd76e9df6b
sanja_susnjar <ssusnj01@fesb.hr>
2024-02-24 17:00:48 +0100

    Bug fixed

--- src/components/Kontakt/Kontakt.jsx
+++ src/components/Kontakt/Kontakt.jsx
@@ -9,13 +9,14 @@ const Kontakt = () => {
       <Section title="Kontakt" />
       <div className={styles.kontakt}>
         <p className={styles.location}>
-          <iframe
+          {/* <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2893.356457850048!2d16.49152282668016!3d43.51575893868141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13355f6b3644e52d%3A0x5dccac0d1bbddb63!2sTrial%20d.o.o.!5e0!3m2!1shr!2shr!4v1696113592367!5m2!1shr!2shr"
             className={styles.googleMap}
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
+          ></iframe> */}
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2893.3547637379247!2d16.48882807599462!3d43.51579427110902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13355f6b3644e52d%3A0x5dccac0d1bbddb63!2sTrial%20d.o.o.!5e0!3m2!1shr!2shr!4v1708714079947!5m2!1shr!2shr" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </p>
         <hr className={styles.line}></hr>
         <About />
